dc81160c85bec305491aca64b06bda785bea3251
swapnil <bhalerao.swapnil11@gmail.com>
2021-09-29 23:03:34 +0530

    6: One Producer, One Consumer problem.

--- threading/main.cpp
+++ threading/main.cpp
@@ -5,62 +5,63 @@
 //  Created by Swapnil Bhalerao on 23/09/21.
 //
 //  use of condition variable.
-//  Print even number by one thread and odd number by other thread.
+//  1_Producer and 1_Consumer problem
 
 #include <iostream>
 #include <thread>
-#include <vector>
+#include <queue>
 #include <mutex>
 using namespace std;
 std::mutex mu;
 std::condition_variable cond_var;
 
-int var = 0;
+std::deque<int> q;
 
-void even_number()
+void producer_one()
 {
-    while (var <= 20)
+    int data = 20;
+    while (data > 0)
     {
-        std::unique_lock<std::mutex> ul(mu);
-        if (var % 2 == 0)
-        {
-            cout << "even thread => " << var << endl;
-            var++;
-            cond_var.notify_one();
-        }
-        else
-        {
-            cout<<"even waiting\n";
-            cond_var.wait(ul);
-        }
+        cout<<"-p-\n";
+        unique_lock<std::mutex> ul(mu);
+        cout<<"produce\n";
+        q.push_front(data);
+        data--;
+        ul.unlock();
+        cond_var.notify_one();
     }
+    cout<<"p";
 }
 
-void odd_number()
+void consumer_one()
 {
-    while (var <= 20)
+    int data = 0;
+    while (data != 1)
     {
-        std::unique_lock<std::mutex> ul(mu);
-        if (var % 2 != 0)
+        cout<<"-c-\n";
+        unique_lock<std::mutex> ul(mu);
+        if (!q.empty())
         {
-            cout << "odd  thread => " << var << endl;
-            var++;
-            cond_var.notify_one();
+            data = q.back();
+            cout << "consume => " << q.back() << endl;
+            q.pop_back();
+            ul.unlock();
         }
         else
         {
-            cout<<"odd  waiting\n";
             cond_var.wait(ul);
         }
     }
+    cout<<"c";
 }
 
 int main(int argc, const char *argv[])
 {
     // insert code here...
 
-    std::thread T1(even_number);
-    std::thread T2(odd_number);
+    std::thread T2(consumer_one);
+    std::thread T1(producer_one);
+    
 
     if (T1.joinable())
     {
