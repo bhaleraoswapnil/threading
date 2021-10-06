fa3fbd293379d73430dcf422643291ad20332eaa
swapnil <bhalerao.swapnil11@gmail.com>
2021-10-06 20:18:41 +0530

    8: future promise: pass value from parent thread to child thread.

--- threading/main.cpp
+++ threading/main.cpp
@@ -4,31 +4,34 @@
 //
 //  Created by Swapnil Bhalerao on 23/09/21.
 //
-//  future: pass value to child thread and return result to main thread.
+//  future promise: pass value from parent thread to child thread.
 
 #include <iostream>
 #include <future>
 using namespace std;
 
-int factorial(int inputVal)
+int factorial(std::future<int> &f)
 {
-//    cout << "Child thread ID :" << std::this_thread::get_id() << endl;
+    //cout << "Child thread ID :" << std::this_thread::get_id() << endl;
     int result = 1;
+    int inputVal = f.get();
     for (int i = 1; i <= inputVal; i++)
     {
         result = result * i;
     }
-    
+
     return result;
 }
 int main(int argc, const char *argv[])
 {
     // insert code here...
-    int x;
-    cout << "Enter value to calculate Factorial\n";
-    cin >> x;
-    std::future<int> fu = std::async(std::launch::async, factorial, x);
-//    cout << "Parent thread ID :" << std::this_thread::get_id() << endl;
-    cout << "Factorial from Child Thread = " << fu.get() << endl;
+    std::promise<int> p; // promise between parent and child thread.
+    std::future<int> f;  // pass future to child thread to get value from parent thread.
+    f = p.get_future();
+
+    std::future<int> fu = std::async(std::launch::async, factorial, std::ref(f));
+    p.set_value(4);
+
+    cout << "factorial == " << fu.get() << endl;
     return 0;
 }
