d0ee3f03e2820cebb0b77dd63a4413c1f3a1bfa4
swapnil <bhalerao.swapnil11@gmail.com>
2021-10-06 18:51:33 +0530

    7: future: Child thread return value to parent thread.

--- threading/main.cpp
+++ threading/main.cpp
@@ -4,73 +4,31 @@
 //
 //  Created by Swapnil Bhalerao on 23/09/21.
 //
-//  use of condition variable.
-//  1_Producer and 1_Consumer problem
+//  future: pass value to child thread and return result to main thread.
 
 #include <iostream>
-#include <thread>
-#include <queue>
-#include <mutex>
+#include <future>
 using namespace std;
-std::mutex mu;
-std::condition_variable cond_var;
 
-std::deque<int> q;
-
-void producer_one()
+int factorial(int inputVal)
 {
-    int data = 20;
-    while (data > 0)
+//    cout << "Child thread ID :" << std::this_thread::get_id() << endl;
+    int result = 1;
+    for (int i = 1; i <= inputVal; i++)
     {
-        cout<<"-p-\n";
-        unique_lock<std::mutex> ul(mu);
-        cout<<"produce\n";
-        q.push_front(data);
-        data--;
-        ul.unlock();
-        cond_var.notify_one();
+        result = result * i;
     }
-    cout<<"p";
+    
+    return result;
 }
-
-void consumer_one()
-{
-    int data = 0;
-    while (data != 1)
-    {
-        cout<<"-c-\n";
-        unique_lock<std::mutex> ul(mu);
-        if (!q.empty())
-        {
-            data = q.back();
-            cout << "consume => " << q.back() << endl;
-            q.pop_back();
-            ul.unlock();
-        }
-        else
-        {
-            cond_var.wait(ul);
-        }
-    }
-    cout<<"c";
-}
-
 int main(int argc, const char *argv[])
 {
     // insert code here...
-
-    std::thread T2(consumer_one);
-    std::thread T1(producer_one);
-    
-
-    if (T1.joinable())
-    {
-        T1.join();
-    }
-
-    if (T2.joinable())
-    {
-        T2.join();
-    }
+    int x;
+    cout << "Enter value to calculate Factorial\n";
+    cin >> x;
+    std::future<int> fu = std::async(std::launch::async, factorial, x);
+//    cout << "Parent thread ID :" << std::this_thread::get_id() << endl;
+    cout << "Factorial from Child Thread = " << fu.get() << endl;
     return 0;
 }
